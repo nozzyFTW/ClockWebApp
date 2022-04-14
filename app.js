70736b41b8b4e86139f42ea1db0d76101f4af151
nozzy <87353282+nozzyFTW@users.noreply.github.com>
2022-04-14 14:32:20 +0930

    adding showMore button

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+/////////////
+//  CLOCK  //
+/////////////
 var noon = 12;
 
 // Getting it to show the current time on the page
@@ -14,7 +17,6 @@ var showCurrentTime = function()
     var meridian = "AM";
  
     // Set hours
-
 	  if (hours > noon)
 	  {
 		  hours = hours - 12;
@@ -23,11 +25,11 @@ var showCurrentTime = function()
     // Set Minutes
     if (minutes < 10)
     {
-        minutes = "0" + minutes;
+      minutes = "0" + minutes;
     }
  
     // put together the string that displays the time
-    var clockTime = hours + ':' + minutes;
+    var clockTime = hours + ":" + minutes;
  
     clock.innerText = clockTime;
 };
@@ -39,3 +41,57 @@ var updateClock = function()
   showCurrentTime();
 };
 setInterval(() => {updateClock()}, 1000);
+
+////////////
+//  DATE  //
+////////////
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var showCurrentDate = function()
+{
+  var date = document.getElementById('date');
+
+  var currentDate = new Date();
+
+  var dayWeek = currentDate.getDay();
+  var dayMonth = currentDate.getDate();
+  var month = currentDate.getMonth();
+  var year = currentDate.getFullYear();
+
+  // Getting name of Day
+  var dayName = days[dayWeek];
+
+  // Getting name of month
+  var monthName = months[month];
+
+  var dateData = dayName + ", " + dayMonth + " " + monthName + " " + year;
+
+  date.innerText = dateData;
+};
+
+var updateDate = function() 
+{
+  showCurrentDate();
+};
+setInterval(() => {updateDate()}, 1000);
+
+////////////////////////
+//  READ MORE BUTTON  //
+////////////////////////
+var showMoreBtn = document.querySelector(".showMore");
+var gamesList = document.querySelector(".gamesList");
+var moreGamesList = document.getElementsByClassName("moreGames");
+
+showMoreBtn.addEventListener("click", (e) => {
+  gamesList.classList.toggle("show-more");
+  if (showMoreBtn.innerHTML === 'Show More <i class="fa fa-chevron-down"></i>')
+  {
+    showMoreBtn.innerHTML = 'Show Less <i class="fa fa-chevron-up"></i>';
+    moreGamesList.removeProperty('display')
+  }
+  else
+  {
+    showMoreBtn.innerHTML = 'Show More <i class="fa fa-chevron-down"></i>';
+  }
+});
